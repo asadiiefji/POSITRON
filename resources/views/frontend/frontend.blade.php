4993d22f67072b14f390bb83f894d7327faf59a2
asadiiefji <81674583+asadiiefji@users.noreply.github.com>
2021-08-28 14:31:36 +0700

    scroll karo icon

--- resources/views/frontend/frontend.blade.php
+++ resources/views/frontend/frontend.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <link rel="shortcut icon" href="img/favicon.png" type="image/png">
+        <link rel="shortcut icon" href="img/favicon1.svg" type="image/png">
 
         <!-- Required meta tags -->
     <meta charset="utf-8">
